906b654ae42f5f87a7f7b1587ba5d8bcd13b015f
lucky-vers <vermalucky2004@gmail.com>
2024-04-23 18:14:13 +0530

    Modified `a02.c`

--- 02 Branching Control Structures/a02.c
+++ 02 Branching Control Structures/a02.c
@@ -6,14 +6,10 @@ int main() {
     printf("Enter number of days the book was late: ");
     scanf("%d", &days);
 
-    if (days == 5)
-        fine = 0.5;
-    else if (days >= 6 && days <= 10)
-        fine = 1;
-    else if (days > 10)
-        fine = 5;
-    else
-        fine = 10;
+    if (days == 5) fine = 0.5;
+    else if (days >= 6 && days <= 10) fine = 1;
+    else if (days > 10) fine = 5;
+    else fine = 10;
 
     printf("The fine you need to pay is %.2f rupees", fine);
 
