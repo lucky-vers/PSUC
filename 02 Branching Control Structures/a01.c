849c57074981c84d6014c8787f9be70eb09ea6d5
lucky-vers <vermalucky2004@gmail.com>
2024-04-23 18:14:20 +0530

    Modified `a01.c`

--- 02 Branching Control Structures/a01.c
+++ 02 Branching Control Structures/a01.c
@@ -5,12 +5,9 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &a);
 
-    if (a > 0)
-        printf("%d is positive\n", a);
-    else if (a < 0)
-        printf("%d is negative\n", a);
-    else
-        printf("%d is zero\n", a);
+    if (a > 0) printf("%d is positive\n", a);
+    else if (a < 0) printf("%d is negative\n", a);
+    else printf("%d is zero\n", a);
 
     return 0;
 }
